e9c0ceff1559d607318c8fd90d5336418e26a991
Nicolas Trangez <ikke@nicolast.be>
2016-01-22 17:44:59 +0100

    cbits: Implement PowerPC AltiVec support
    This commit adds PowerPC AltiVec support to `reedsolomon.c` within the
    existing framework.
    
    Note two slightly different implementations are provided, dispatched in
    a `configure` check: one using the built-in compiler support for
    right-shift of a vector (which should work on any target system),
    another one using the low-level `vec_vsrd` intrinsic, which is only
    available on POWER8 systems. As such it is important to export the
    right flags (e.g. `CPPFLAGS='-mcpu=power8'`) when building the package.
    
    Unlike the Intel implementations, no runtime support check is performed,
    and using the library on an incompatible system will result in crashes.

--- cbits/compare-isa.sh
+++ cbits/compare-isa.sh
@@ -21,11 +21,15 @@ function test_arch() {
     local isa=$1
     shift
     local backend=$1
+    local qemu_cpu=''
+    if test -n ${2:-''}; then
+        qemu_cpu="-cpu ${2}"
+    fi
 
-    echo "Validating $isa with backend $backend"
+    echo "Validating $isa with backend $backend on ${2:-generic}"
     stack runhaskell reedsolomon-gal-mul-stdio-quickcheck.hs -- \
         ./reedsolomon-gal-mul-stdio-$HOST_ISA-native \
-        "qemu-$qemu_arch -L ~/x-tools/$isa/$isa/sysroot ./reedsolomon-gal-mul-stdio-$isa-$backend"
+        "qemu-$qemu_arch $qemu_cpu -L ~/x-tools/$isa/$isa/sysroot ./reedsolomon-gal-mul-stdio-$isa-$backend"
     echo
 }
 
@@ -35,8 +39,10 @@ build_for_arch armv7-rpi2-linux-gnueabihf neon '-mfpu=neon'
 # My PPC64 x-tools require `-static` for executables to work with `qemu-ppc64`
 build_for_arch powerpc64-unknown-linux-gnu generic '-static'
 build_for_arch powerpc64-unknown-linux-gnu altivec '-static -maltivec'
+build_for_arch powerpc64-unknown-linux-gnu altivec-power8 '-static -mcpu=power8'
 
 test_arch arm armv7-rpi2-linux-gnueabihf generic
 test_arch arm armv7-rpi2-linux-gnueabihf neon
 test_arch ppc64 powerpc64-unknown-linux-gnu generic
 test_arch ppc64 powerpc64-unknown-linux-gnu altivec
+test_arch ppc64 powerpc64-unknown-linux-gnu altivec-power8 POWER8
