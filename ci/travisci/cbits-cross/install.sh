e47c38825f6775d279a89e1623ead0ec078c05c0
Nicolas Trangez <ikke@nicolast.be>
2016-01-24 17:58:45 +0100

    TravisCI: Fix non-cached build and use `$RESOLVER`
    The `cbits-cross` test suite doesn't correctly run `stack setup` in
    order to install GHC etc. This wasn't detected before because of a build
    cache being reused.

--- ci/travisci/cbits-cross/install.sh
+++ ci/travisci/cbits-cross/install.sh
@@ -6,5 +6,11 @@ popd
 
 stack \
     --no-terminal \
+    setup \
+    --resolver=$RESOLVER
+
+stack \
+    --no-terminal \
     install \
+    --resolver=$RESOLVER \
     bytestring process process-extras QuickCheck
