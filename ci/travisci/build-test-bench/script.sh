7c90360d1d888c241247e7a5ad5d86c9404eeaec
Nicolas Trangez <ikke@nicolast.be>
2016-09-17 23:56:12 +0200

    TravisCI: Enable Stackage LTS 6 & 7
    Also bump the MacOS X Stackage version to LTS 7.

--- ci/travisci/build-test-bench/script.sh
+++ ci/travisci/build-test-bench/script.sh
@@ -3,26 +3,26 @@
 echo "Running tests with SIMD support disabled"
 stack --no-terminal build \
         --flag=reedsolomon:-SIMD \
-        ${STACK_BUILD_OPTIONS[*]:-} \
         --flag=reedsolomon:examples \
         --pedantic \
         --test \
         --test-arguments "+RTS -N2" \
         --bench \
         --no-run-benchmarks \
-        --resolver=$RESOLVER
+        --resolver=$RESOLVER \
+        ${STACK_BUILD_OPTIONS[*]:-}
 
 stack --resolver=$RESOLVER clean
 
 echo "Running tests with default settings (SIMD support enabled)"
 stack --no-terminal build \
-        ${STACK_BUILD_OPTIONS[*]:-} \
         --flag=reedsolomon:examples \
         --pedantic \
         --test \
         --test-arguments "+RTS -N2" \
         --bench \
-        --resolver=$RESOLVER
+        --resolver=$RESOLVER \
+        ${STACK_BUILD_OPTIONS[*]:-}
 
 stack --resolver=$RESOLVER exec reedsolomon-simple-bench 5 2 $(( 5 * 1024 * 1024 ))
 stack --resolver=$RESOLVER exec reedsolomon-simple-bench 10 2 $(( 10 * 1024 * 1024 ))
