a053381181949a70827df30fd7422f3940eeff33
Nicolas Trangez <ikke@nicolast.be>
2017-01-18 17:40:40 +0100

    Stash

--- cbits/tests/reedsolomon-gal-mul-stdio.c
+++ cbits/tests/reedsolomon-gal-mul-stdio.c
@@ -25,9 +25,11 @@
 # include "config.h"
 #endif
 
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <strings.h>
 
 #if defined(_WIN32) && _WIN32
 # define USE_WIN32 1
@@ -98,6 +100,96 @@ static int write_all(const int fd, const uint8_t *vec, size_t count) {
         return 0;
 }
 
+typedef PROTO_RETURN(*reedsolomon_gal_mul_fun)(PROTO_ARGS);
+
+static reedsolomon_gal_mul_fun get_impl(reedsolomon_cpu_support type) {
+        reedsolomon_gal_mul_fun res = NULL;
+
+#define CASE(selector, impl) \
+        case selector: \
+                res = impl; \
+                break;
+
+        switch(type) {
+#if RS_HAVE_GENERIC
+                CASE(REEDSOLOMON_CPU_GENERIC, reedsolomon_gal_mul_generic)
+#endif
+#if RS_HAVE_SSE2
+                CASE(REEDSOLOMON_CPU_SSE2, reedsolomon_gal_mul_sse2)
+#endif
+#if RS_HAVE_SSSE3
+                CASE(REEDSOLOMON_CPU_SSSE3, reedsolomon_gal_mul_ssse3)
+#endif
+#if RS_HAVE_AVX
+                CASE(REEDSOLOMON_CPU_AVX, reedsolomon_gal_mul_avx)
+#endif
+#if RS_HAVE_AVX2
+                CASE(REEDSOLOMON_CPU_AVX2, reedsolomon_gal_mul_avx2)
+#endif
+#if RS_HAVE_NEON
+                CASE(REEDSOLOMON_CPU_NEON, reedsolomon_gal_mul_neon)
+#endif
+#if RS_HAVE_ALTIVEC
+                CASE(REEDSOLOMON_CPU_ALTIVEC, reedsolomon_gal_mul_altivec)
+#endif
+                default:
+                        res = NULL;
+                        break;
+        }
+#undef CASE
+
+        return res;
+}
+
+static const char * impl_name(reedsolomon_cpu_support type) {
+        const char * res = NULL;
+
+        switch(type) {
+                case REEDSOLOMON_CPU_GENERIC:
+                        res = "Generic";
+                        break;
+                case REEDSOLOMON_CPU_SSE2:
+                        res = "SSE2";
+                        break;
+                case REEDSOLOMON_CPU_SSSE3:
+                        res = "SSSE3";
+                        break;
+                case REEDSOLOMON_CPU_AVX:
+                        res = "AVX";
+                        break;
+                case REEDSOLOMON_CPU_AVX2:
+                        res = "AVX2";
+                        break;
+                case REEDSOLOMON_CPU_NEON:
+                        res = "NEON";
+                        break;;
+                case REEDSOLOMON_CPU_ALTIVEC:
+                        res = "AltiVec";
+                        break;
+        }
+
+        return res;
+}
+
+static int parse_backend(const char *name, reedsolomon_cpu_support *id) {
+        int rc = -1;
+
+#define CASE(_name) \
+        if(strcasecmp(name, #_name) == 0) { \
+                *id = REEDSOLOMON_CPU_ ## _name; \
+                rc = 0; \
+        }
+
+        CASE(GENERIC)
+        else CASE(SSE2)
+        else CASE(SSSE3)
+        else CASE(AVX)
+        else CASE(AVX2)
+        else CASE(NEON)
+        else CASE(ALTIVEC)
+
+        return rc;
+}
 
 int main(int argc, char **argv) {
         int rc = 1;
@@ -107,23 +199,70 @@ int main(int argc, char **argv) {
                 *out = NULL,
                 low_vector[16] = { 0 },
                 high_vector[16] = { 0 };
+        reedsolomon_gal_mul_fun impl = NULL;
+        reedsolomon_cpu_support backend_id = REEDSOLOMON_CPU_GENERIC,
+                                supported = REEDSOLOMON_CPU_GENERIC;
+        const char *backend = "native";
+        char c = 0;
 
-        if(argc != 2) {
-                fprintf(stderr, "Usage: %s SIZE\n", argv[0]);
-                rc = 1;
+        while((c = getopt(argc, argv, "b:")) != -1) {
+                switch(c) {
+                        case 'b':
+                                backend = optarg;
+                                break;
+                        case '?': {
+                                rc = 99;
+                                goto out;
+                        } break;
+                        default:
+                                abort();
+                                break;
+                }
+        }
+
+        if(argc - optind != 1) {
+                fprintf(stderr, "Usage: %s [-b backend] SIZE\n", argv[0]);
+                rc = 99;
                 goto out;
         }
 
-        rc = sscanf(argv[1], "%" SCNsize_t "u", &size);
+        supported = reedsolomon_determine_cpu_support();
+
+        if(strcasecmp(backend, "native") == 0) {
+                fprintf(stderr, "Using native backend, which is %s\n", impl_name(supported));
+                impl = reedsolomon_gal_mul;
+        } else {
+                rc = parse_backend(backend, &backend_id);
+                if(rc != 0) {
+                        fprintf(stderr, "Unknown backend: %s\n", backend);
+                        rc = 99;
+                        goto out;
+                }
+
+                if(backend_id > supported) {
+                        fprintf(stderr, "Requested backend not supported on system CPU: %s > %s\n",
+                                impl_name(backend_id), impl_name(supported));
+                        rc = 77;
+                        goto out;
+                }
+                impl = get_impl(backend_id);
+        }
+        if(impl == NULL) {
+                fprintf(stderr, "Failed to find requested implementation\n");
+                rc = 99;
+                goto out;
+        }
+
+        rc = sscanf(argv[optind], "%" SCNsize_t "u", &size);
         if(rc == EOF) {
                 perror("sscanf");
-                rc = 1;
+                rc = 99;
                 goto out;
         }
 
         if(size == 0) {
                 fprintf(stderr, "Invalid size: %" PRIsize_t "u\n", size);
-                rc = 1;
+                rc = 99;
                 goto out;
         }
 
@@ -131,60 +270,60 @@ int main(int argc, char **argv) {
         rc = _setmode(STDIN_FILENO, _O_BINARY);
         if(rc == -1) {
                 perror("_setmode");
-                rc = 1;
+                rc = 99;
                 goto out;
         }
 
         rc = _setmode(STDOUT_FILENO, _O_BINARY);
         if(rc == -1) {
                 perror("_setmode");
-                rc = 1;
+                rc = 99;
                 goto out;
         }
 #endif
 
         rc = read_all(STDIN_FILENO, low_vector, sizeof(low_vector));
         if(rc != 0) {
-                rc = 1;
+                rc = 99;
                 goto out;
         }
 
         rc = read_all(STDIN_FILENO, high_vector, sizeof(high_vector));
         if(rc != 0) {
-                rc = 1;
+                rc = 99;
                 goto out;
         }
 
         data = malloc(size);
         if(data == NULL) {
                 perror("malloc");
-                rc = 1;
+                rc = 99;
                 goto out;
         }
 
         rc = read_all(STDIN_FILENO, data, size);
         if(rc != 0) {
-                rc = 1;
+                rc = 99;
                 goto out;
         }
 
         out = malloc(size);
         if(out == NULL) {
                 perror("malloc");
-                rc = 1;
+                rc = 99;
                 goto out;
         }
 
-        cnt = reedsolomon_gal_mul(low_vector, high_vector, data, out, size);
+        cnt = impl(low_vector, high_vector, data, out, size);
         if(cnt != size) {
                 fprintf(stderr, "Count mismatch: size=%" PRIsize_t "u, cnt=%" PRIsize_t "u\n", size, cnt);
-                rc = 1;
+                rc = 99;
                 goto out;
         }
 
         rc = write_all(STDOUT_FILENO, out, size);
         if(rc != 0) {
-                rc = 1;
+                rc = 99;
                 goto out;
         }
 
--- cbits/tests/run-test.sh
+++ cbits/tests/run-test.sh
@@ -7,4 +7,61 @@ INPUT=$1
 SIZE=$(( ($(wc -c "${INPUT}" | awk '{ print $1 }') - 16 - 16) / 2 ))
 REFERENCE_OFFSET=$(( SIZE + 16 + 16 ))
 
-./reedsolomon-gal-mul-stdio ${SIZE} < "${INPUT}" | cmp -i ${REFERENCE_OFFSET}:0 "${INPUT}"
+function do_test() {
+        local cnt=$1
+        shift
+        local kind=$1
+        shift
+        local lower_kind
+        lower_kind=$( echo "${kind}" | tr '[:upper:]' '[:lower:]' )
+        local output
+        output=$( mktemp )
+
+        echo "# Settings:"
+        echo "#     kind=${kind}"
+        echo "#     lower_kind=${lower_kind}"
+        echo "#     INPUT=${INPUT}"
+        echo "#     SIZE=${SIZE}"
+        echo "#     REFERENCE_OFFSET=${REFERENCE_OFFSET}"
+        echo "#     output=${output}"
+
+        set +e
+        ./reedsolomon-gal-mul-stdio -b "${lower_kind}" "${SIZE}" < "${INPUT}" > "${output}"
+        local RC=$?
+        set -e
+
+        case "${RC}" in
+                "0")
+                        set +e
+                        cmp -i "${REFERENCE_OFFSET}:0" "${INPUT}" "${output}"
+                        local RC=$?
+                        set -e
+                        rm -f "${output}"
+                        case "${RC}" in
+                                "0")
+                                        echo "ok ${cnt} - ${kind}";;
+                                *)
+                                        echo "not ok ${cnt} - ${kind} (cmp failure, rc=${RC})";;
+                        esac;;
+                "77")
+                        rm -f "${output}"
+                        echo "ok ${cnt} - ${kind} # SKIP Instructions not supported";;
+                "99")
+                        rm -f "${output}"
+                        echo "Bail out! Failure during reedsolomon-gal-mul-stdio execution";;
+                *)
+                        rm -f "${output}"
+                        echo "not ok ${cnt} - ${kind} (reedsolomon-gal-mul-stdio failure, rc=${RC})";;
+        esac
+}
+
+echo 1..8
+echo "# Calculated size for test ${INPUT}: ${SIZE}"
+do_test 1 Native
+do_test 2 Generic
+do_test 3 SSE2
+do_test 4 SSSE3
+do_test 5 AVX
+do_test 6 AVX2
+do_test 7 Neon
+do_test 8 AltiVec
