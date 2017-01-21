37ae5f7f996508e70ed1e0d5093b2ec7d351b78a
Nicolas Trangez <ikke@nicolast.be>
2017-01-21 22:23:59 +0100

    cbits: test-reedsolomon-gal-mul-xor

---
+++ cbits/tests/test-reedsolomon-gal-mul-xor.c
@@ -0,0 +1,69 @@
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "reedsolomon.h"
+
+#define LEN (64)
+#define SEED (42)
+
+/* Simple Lehmer PRNG */
+static inline uint32_t next(uint32_t rnd) {
+    return ((uint64_t)rnd * 279470273UL) % 4294967291UL;
+}
+
+static uint32_t fill_random(void *ptr, size_t len, uint32_t seed) {
+        uint32_t rnd = seed;
+        size_t i = 0;
+
+        for(i = 0; i < len / 4; i++) {
+                rnd = next(rnd);
+                memcpy((char *)ptr + (i * 4), &rnd, 4);
+        }
+
+        rnd = next(rnd);
+        memcpy((char *)ptr + (i * 4), &rnd, len - (i * 4));
+
+        return rnd;
+}
+
+int main() {
+        uint8_t low[16] = { 0 },
+                high[16] = { 0 },
+                data[LEN] = { 0 },
+                out[LEN] = { 0 },
+                initial_out[LEN] = { 0 },
+                out_xor[LEN];
+        size_t size = 0,
+               idx = 0;
+        int rc = 1;
+        uint32_t rnd = SEED;
+
+        rnd = fill_random(low, sizeof(low), rnd);
+        rnd = fill_random(high, sizeof(high), rnd);
+        rnd = fill_random(data, sizeof(data), rnd);
+        fill_random(initial_out, sizeof(initial_out), rnd);
+
+        memcpy(out_xor, initial_out, LEN);
+
+        size = reedsolomon_gal_mul_xor(low, high, data, out_xor, sizeof(data));
+        if(size != LEN) {
+                abort();
+        }
+
+        size = reedsolomon_gal_mul(low, high, data, out, sizeof(data));
+        if(size != LEN) {
+                abort();
+        }
+
+        for(idx = 0; idx < sizeof(out_xor); idx++) {
+                if(out_xor[idx] != (out[idx] ^ initial_out[idx])) {
+                        rc = 1;
+                        goto out;
+                }
+        }
+
+        rc = 0;
+out:
+        return rc;
+}
